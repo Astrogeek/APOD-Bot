aecffe714b96589aebf1904d7cfd82bfdf061efb
Taufeeq <taufeeq.riyaz64@gmail.com>
2022-10-01 23:20:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,9 @@
+from os import name
 import discord
+import aiohttp
+import requests
+from discord_components import*
+from discord import flags
+from discord.ext import commands, tasks
 
 client = commands.Bot(command_prefix=commands.when_mentioned_or('.'))
